2d9d5029e34a8d05c0143222bcad9965e97608e2
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-04 06:52:31 +0530

    insert, clean, back added

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+function dom  (){
+  return texts = document.querySelector(".textview")
 
+}
 function insert(num){
-  
+  const text = dom();
+
+    text.value += num;
   }
   
   function equals(){
     
+      
   }
   
   function clean(){
-    
+    const text = dom()
+    text.value = '';
   }
   
   function back(){
-    
+    const text = dom()
+    text.value = text.value.slice(0,-1)
   }
